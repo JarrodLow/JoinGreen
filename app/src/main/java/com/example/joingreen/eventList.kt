1266b31239917aa9ec598380ef24da5e6a9c0e7d
SoonYiLim <56568354+SoonYiLim@users.noreply.github.com>
2019-12-28 02:39:38 +0800

    SignUp LogIn Reward Event(Pending)

---
+++ app/src/main/java/com/example/joingreen/eventList.kt
@@ -0,0 +1,13 @@
+package com.example.joingreen
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+
+class eventList {
+
+}
