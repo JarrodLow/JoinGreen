b9b9060cf7d13bcc211f8bf3817cf9e8ab4f07fb
SoonYiLim <56568354+SoonYiLim@users.noreply.github.com>
2020-01-11 14:11:25 +0800

    SignUp LogIn Reward Event Attendance Create Event Profile EventDetail Date Kotlin(Pending) Time Kotlin(Pending)

---
+++ app/src/main/java/com/example/joingreen/createEvent.kt
@@ -0,0 +1,79 @@
+package com.example.joingreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.create_event.*
+import java.util.*
+
+class createEvent : AppCompatActivity() {
+
+    var username = "username"   //Need to get username with session?
+    lateinit var editTextEventName : EditText
+    lateinit var editTextEventDate : EditText
+    lateinit var editTextStartTime : EditText
+    lateinit var editTextEndTime : EditText
+    lateinit var editTextLocation : EditText
+    lateinit var imageViewEventPhoto: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.create_event)
+
+        //save photo variables
+        imageViewEventPhoto=findViewById(R.id.addPic)
+        editTextEventName= findViewById(R.id.eventName)
+        editTextEventDate=findViewById(R.id.eventDate)
+        editTextStartTime=findViewById(R.id.startTime)
+        editTextEndTime=findViewById(R.id.endTime)
+        editTextLocation=findViewById(R.id.location)
+
+        createEvent.setOnClickListener {
+            addEvent(it)
+            clearForm(it)
+        }
+
+    }
+
+    private fun addEvent(view: View){
+        //save all user entered details
+        val eventPhoto=imageViewEventPhoto.
+        val eventCreator=username;
+        val eventName=editTextEventName.text.toString().trim()
+        val eventDate=editTextEventDate.text.toString().trim()
+        val eventStartTime=editTextStartTime.text.toString()
+        val eventEndTime=editTextEndTime.text.toString()
+        val eventLocation=editTextLocation.text.toString()
+        val attendance=(100000..999999).random()
+
+        //link firebase
+        val createEventDB=FireBaseDatabase.getInstance().getReference("Event") //firebase havent link
+        val eventId=createEventDB.push().key
+
+        if(eventCreator==null && eventName==null && eventDate==null && eventStartTime==null && eventEndTime==null && eventLocation==null){
+            Toast.makeText(applicationContext,"Please fill in all required fields", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        //save all data into event object
+        val newEvent=eventClass(eventId,eventCreator,eventName,eventDate,eventStartTime,eventEndTime,eventLocation,attendance)
+
+        //add data into database
+        createEventDB.child(eventId).setValue(newEvent).addOnCompleteListener{
+            Toast.makeText(applicationContext,"Event has been created successfully", Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+    private fun clearForm(view: View){
+        addPic.setImageBitmap(null)
+        eventName.text.clear()
+        eventDate.text.clear()
+        startTime.text.clear()
+        endTime.text.clear()
+        location.text.clear()
+    }
+}
