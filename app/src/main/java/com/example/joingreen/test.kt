92a445d71fc1f7a2b03e43f644dcc9d2744a9e4d
SoonYiLim <56568354+SoonYiLim@users.noreply.github.com>
2020-01-12 11:56:15 +0800

    SignUp LogIn Reward Event Attendance Create Event Profile EventDetail Date Kotlin(Pending) Time Kotlin(Pending)

---
+++ app/src/main/java/com/example/joingreen/test.kt
@@ -0,0 +1,13 @@
+package com.example.joingreen
+
+import android.os.Bundle
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+
+class test :AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.create_event)
+    }
+}
