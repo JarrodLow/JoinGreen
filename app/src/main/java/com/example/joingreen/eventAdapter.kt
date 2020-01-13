ed07f8e1589289a14e83ca82b849fe2de6fbb7fa
LegendFatboy <lowjar20@gmail.com>
2020-01-13 13:20:25 +0800

    asd

--- app/src/main/java/com/example/joingreen/eventAdapter.kt
+++ app/src/main/java/com/example/joingreen/eventAdapter.kt
@@ -13,12 +13,12 @@ class eventAdapter(val mCtx: Context,val layoutResId: Int,val eventList: List<ev
     : ArrayAdapter<eventClass>(mCtx, layoutResId, eventList) {
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-
+        var username = ""
         lateinit var showEventDB: DatabaseReference
-        var auth: FirebaseAuth?=null
+        var auth: FirebaseAuth? = null
 
         val layoutInflater: LayoutInflater = LayoutInflater.from(mCtx)
-        val view: View = layoutInflater.inflate(layoutResId,null)
+        val view: View = layoutInflater.inflate(layoutResId, null)
 
         val textViewEventName = view.findViewById<TextView>(R.id.eventName)
         val textViewEventCreator = view.findViewById<TextView>(R.id.eventCreator)
@@ -26,57 +26,60 @@ class eventAdapter(val mCtx: Context,val layoutResId: Int,val eventList: List<ev
         val textViewEventTime = view.findViewById<TextView>(R.id.eventTime)
         val textViewEventLocation = view.findViewById<TextView>(R.id.location)
         val textViewAttCode = view.findViewById<TextView>(R.id.attdCode)
-        val joinedButton=view.findViewById<Button>(R.id.joinBtn)
+        val joinedButton = view.findViewById<Button>(R.id.joinBtn)
 
         val event = eventList[position]
 
-        textViewEventName.text=event.eventName
-        textViewEventCreator.text=event.creator
-        textViewEventDate.text=event.eventDate
-        textViewEventTime.text=event.eventStartTime
-        textViewEventLocation.text=event.eventLocation
-        textViewAttCode.text=event.attCode.toString()
+        textViewEventName.text = event.eventName
+        textViewEventCreator.text = event.creator
+        textViewEventDate.text = event.eventDate
+        textViewEventTime.text = event.eventStartTime
+        textViewEventLocation.text = event.eventLocation
+        textViewAttCode.text = event.attCode.toString()
+        var database: DatabaseReference =
+            FirebaseDatabase.getInstance().reference.child("/users")
+        auth = FirebaseAuth.getInstance()
+        val User = auth!!.currentUser
+        val UserReference = database!!.child(User!!.uid)
 
+
+        UserReference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                val nameHolder = snapshot.child("userName").value as String
+                username = nameHolder
+
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+            }
+        })
         joinedButton.setOnClickListener {
 
 
-           // var database :DatabaseReference = FirebaseDatabase.getInstance().reference.child("/users")
-            auth = FirebaseAuth.getInstance()
-            showEventDB=FirebaseDatabase.getInstance().reference.child("/users")
-            val User = auth!!.currentUser
-            val UserReference = showEventDB!!.child(User!!.uid)
-            var username=""
+            // var database :DatabaseReference = FirebaseDatabase.getInstance().reference.child("/users")
 
-            UserReference.addValueEventListener(object : ValueEventListener{
-                override fun onDataChange(p0: DataSnapshot) {
 
-                    val nameHolder = p0.child("userName").value as String
-                    username=nameHolder
+            val joinEventName = textViewEventName.text.toString()
+            val createEventDB: DatabaseReference =
+                FirebaseDatabase.getInstance().getReference("JoinedUser")
+            val joinedUserId = createEventDB.push().key.toString()
 
-                }
+            val joinedEventUser = joinedUser(joinedUserId, joinEventName, username)
 
-                override fun onCancelled(p0: DatabaseError) {
-                }
-            })
-
-            val joinEventName=textViewEventName.text.toString()
-
-            val userJoinEventDB=FirebaseDatabase.getInstance().getReference("JoinedUser")
-            val joinedUserId=userJoinEventDB.push().key.toString()
-            val joinedEventUser=joinedUser(joinedUserId,joinEventName,username)
-
-            userJoinEventDB.child(joinedUserId).setValue(joinedEventUser).addOnCanceledListener {
+            createEventDB.child(joinedUserId).setValue(joinedEventUser).addOnCanceledListener {
 
             }
         }
 
+            return view
 
-        return view
 
-
+        }
     }
 
 
 
-}
+
+
 
